39d88fd781a5dcfe15693b4f746a5b22c92c4643
Ryan Bird <ryan01bird@gmail.com>
2015-08-26 15:36:22 -0600

    used Number & strings to return decimal

--- app.js
+++ app.js
@@ -31,31 +31,53 @@ var express = require('express'),
   //   res.send(result.toString());
   // }); 
 
-  app.get("/:operator/:num1/:num2", function(req, res){
-    var result;
-    if (req.params.operator === 'div') {
-      result = parseInt(req.params.num1) / parseInt(req.params.num2)
-    }
+  // app.get("/:operator/:num1/:num2", function(req, res){
+  //   var result;
+  //   if (req.params.operator === 'div') {
+  //     result = parseInt(req.params.num1) / parseInt(req.params.num2)
+  //   }
 
-    if (req.params.operator === 'mult') {
-      result = parseInt(req.params.num1) * parseInt(req.params.num2)
-    }
+  //   if (req.params.operator === 'mult') {
+  //     result = parseInt(req.params.num1) * parseInt(req.params.num2)
+  //   }
 
-    if (req.params.operator === 'sub') {
-      result = parseInt(req.params.num1) - parseInt(req.params.num2)
-    }
+  //   if (req.params.operator === 'sub') {
+  //     result = parseInt(req.params.num1) - parseInt(req.params.num2)
+  //   }
 
-    if (req.params.operator === 'add') {
-      result = parseInt(req.params.num1) + parseInt(req.params.num2)
-    }
+  //   if (req.params.operator === 'add') {
+  //     result = parseInt(req.params.num1) + parseInt(req.params.num2)
+  //   }
  
-    res.send(result.toString())
-  })
+  //   res.send(result.toString())
+  // })
+
+app.get('/:operator/:num1/:num2', function (req, res) {
+  var operator = (req.params.operator);
+  var num1 = Number(req.params.num1);
+  var num2 = Number(req.params.num2);
+  if (operator === 'add') {
+    var addition = num1 + num2;
+    res.send(req.params.num1 + " + " + req.params.num2 + " = " + addition);
+  }
+  if (operator === 'sub') {
+    var subtraction = num1 - num2;
+    res.send(req.params.num1 + " - " + req.params.num2 + " = " + subtraction);
+  }
+  if (operator === 'mult') {
+    var multiplication = num1 * num2;
+    res.send(req.params.num1 + " * " + req.params.num2 + " = " + multiplication);
+  }
+  if (operator === 'div') {
+    var division = num1 / num2;
+    res.send(req.params.num1 + " / " + req.params.num2 + " = " + division);
+  }
+})
 
 app.listen(3000, function () {
   console.log("Go to localhost:3000/");
 });
-
+   
 // Using res.send, make the following work:
 // When a user visits /add/9/3, it should display 12
 // When a user visits /sub/9/3, it should display 6
